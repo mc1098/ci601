c4a2b31e9faaf3542ef6d2a91a980429b233901c
mc1098 <m.cripps1@uni.brighton.ac.uk>
2022-01-02 19:52:26 +0000

    Fix use new ast IR for `Parser` trait (#14)

--- src/parse.rs
+++ src/parse.rs
@@ -1,24 +1,25 @@
-//! Trait for parsing generics types to `Bibliography`.
+//! Trait for parsing generics types to `Biblio`.
 
 // TODO: expand on mod docs
 
-use biblatex::Bibliography;
+use crate::Biblio;
+
 use eyre::Result;
 
-/// A trait that performs parsing of the generic type to the `Bibliography`.
+/// A trait that performs parsing of the generic type to the `Biblio`.
 pub trait Parser<T> {
-    /// Parse a generic type to the `Bibliography`.
+    /// Parse a generic type to the `Biblio`.
     ///
     /// # Errors
-    /// When the generic type cannot be parsed into a valid `Bibliography`.
-    fn parse(&self, src: T) -> Result<Bibliography>;
+    /// When the generic type cannot be parsed into a valid `Biblio`.
+    fn parse(&self, src: T) -> Result<Biblio>;
 }
 
 impl<F, T> Parser<T> for F
 where
-    F: Fn(T) -> Result<Bibliography>,
+    F: Fn(T) -> Result<Biblio>,
 {
-    fn parse(&self, src: T) -> Result<Bibliography> {
+    fn parse(&self, src: T) -> Result<Biblio> {
         self(src)
     }
 }
