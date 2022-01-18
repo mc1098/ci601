089f973299d5da190f89e24dad33882fbd451897
mc1098 <m.cripps1@uni.brighton.ac.uk>
2022-01-18 19:19:58 +0000

    Fix lib docs using old name (#60)

--- crates/seb/src/lib.rs
+++ crates/seb/src/lib.rs
@@ -9,10 +9,10 @@
 #![warn(missing_docs)]
 #![allow(clippy::module_name_repetitions)]
 
-//! # bibadd-core
+//! # seb
 //!
-//! bibadd-core is a library which supports searching for bibliography entries from select APIs
-//! and adding them to an in memory bibliography. bibadd-core supports transforming the in-memory
+//! seb is a library which supports searching for bibliography entries from select APIs
+//! and adding them to an in memory bibliography model. seb supports transforming the in-memory
 //! bibliography to [`format::Format`]s such as [`format::BibTex`].
 
 mod api;
