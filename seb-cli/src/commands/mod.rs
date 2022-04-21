e8eba6767f52195da2f62a0237e96abc824003d0
mc1098 <m.cripps1@uni.brighton.ac.uk>
2022-04-21 13:59:44 +0100

    Add new `derive` command
    The `derive` command can be used to create a new entry type based on an
    existing entry found in the bibliography file.
    The deriving from an entry creates a new resolver of the new entry type
    and sets the fields from the existing entry for that resolver then the
    outstanding required fields must be resolved by the user.

--- seb-cli/src/commands/mod.rs
+++ seb-cli/src/commands/mod.rs
@@ -27,6 +27,49 @@ pub enum Commands {
     /// set using `-i` or `--interact`.
     Check,
 
+    /// Derive a new entry from an existing one.
+    ///
+    /// This can most commonly be used to make a new entry like a "book chapter" entry based on
+    /// an existing "book" entry.
+    Derive {
+        /// The citation key of the entry to derive from.
+        entry: String,
+
+        /// The kind of entry to add.
+        ///
+        /// The following are known entry types:
+        ///
+        /// - article
+        /// - book
+        /// - booklet
+        /// - book chapter
+        /// - book pages
+        /// - book section
+        /// - in proceedings
+        /// - manual
+        /// - master thesis
+        /// - phd thesis
+        /// - proceedings
+        /// - tech report
+        /// - unpublished
+        ///
+        /// Known entry types will require certain fields to be valid
+        /// and if another kind entry is found then this will be a custom
+        /// entry that only requires a title.
+        #[clap(parse(from_str), verbatim_doc_comment)]
+        kind: seb::ast::EntryKind<'static>,
+
+        /// Cite to use for new entry
+        cite: String,
+
+        /// Require addition fields ontop of what is already required by the kind of entry.
+        ///
+        /// Any fields already required by the kind are ignored so can be added if essential
+        /// when unsure if the kind already requires that field.
+        #[clap(long, multiple_values(true))]
+        fields: Option<Vec<String>>,
+    },
+
     /// Add a new entry manually
     ///
     /// This subcommand will assume interact flag is set even if no explicitly used.
@@ -86,6 +129,34 @@ impl Commands {
             // trivially if the biblio is already resolved at this point then it was either
             // resolved interactively or was valid so a success message can be returned.
             Commands::Check => Ok("All entries contain the required fields!".to_owned()),
+            Commands::Derive {
+                entry,
+                kind,
+                cite,
+                fields,
+            } => {
+                let mut resolver = seb::ast::Entry::resolver_with_cite(kind, cite);
+
+                if let Some(entry) = biblio.get(&entry) {
+                    resolver.set_fields_from_entry(entry);
+                } else {
+                    return Err(eyre::eyre!("No entry found with the cite key of '{entry}'").into());
+                }
+
+                if let Some(fields) = fields {
+                    resolver.add_required_fields(fields);
+                }
+
+                interact::user_resolve_entry(&mut resolver)?;
+                let derived_entry = resolver.resolve()?;
+                let cite = derived_entry.cite().to_owned();
+                biblio.insert(derived_entry);
+
+                log::info!(
+                    "Entry with cite '{cite}' derived from '{entry}' and added to bibliography"
+                );
+                Ok(cite)
+            }
             Commands::New { kind, cite, fields } => {
                 let mut resolver = if let Some(cite) = cite {
                     seb::ast::Entry::resolver_with_cite(kind, cite)
